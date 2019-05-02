021674bc515266ec433f9f742f55e5e737f3b16b
KIM TAE-HYUN <immoowmoow@gmail.com>
2019-05-02 01:31:44 +0000

    .

--- packages/emacs-pkg-lsp-ui.el
+++ packages/emacs-pkg-lsp-ui.el
@@ -6,4 +6,7 @@
   ;; (lsp-ui-peek-find-workspace-symbol "pattern 0")
   ;; If the server supports custom cross references
   ;; (lsp-ui-peek-find-custom 'base "$cquery/base")
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-flycheck-enable t)
   )
