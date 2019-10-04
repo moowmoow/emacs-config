639d08b15f27ed904b2cd026ddecbc7da65d6f56
KIM TAE-HYUN <immoowmoow@gmail.com>
2019-10-04 09:09:14 +0000

    .

--- packages/emacs-pkg-whitespace.el
+++ packages/emacs-pkg-whitespace.el
@@ -7,4 +7,7 @@
   ;; 불필요한 whitespace가 생성되면 whitespace 하이라이트
   (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace t)))
   ;; whitepsace 모드 토클
-  (global-set-key (kbd "C-c w") 'whitespace-mode))
+  (global-set-key (kbd "C-c w") 'whitespace-mode)
+  (global-set-key (kbd "C-c M-w c") 'whitespace-cleanup)
+  (global-set-key (kbd "C-c M-w r") 'whitespace-cleanup-region)
+  )
